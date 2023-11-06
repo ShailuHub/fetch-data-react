44d5f7450a760b30d3875afb4e89e2c7cc7676b2
Shailu <shailesh.respond@gmail.com>
2023-11-06 18:30:05 +0530

    Added useEffect useCallback hooks for first time data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
-
+import React, { useCallback, useEffect, useState } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  async function fetchMovies() {
+  const [error, setError] = useState(null);
+  const [retrying, setRetrying] = useState(true);
+  const [retryTimeout, setRetryTimeout] = useState(null);
+
+  const fetchMovies = useCallback(async () => {
+    setError(null);
     setIsLoading(true);
     try {
       const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
-        throw new Error("Failed to fetch data");
+        throw new Error("Something went wrong ....Retrying");
       }
       const data = await response.json();
       const moviesData = data.results.map((movie) => {
@@ -21,13 +25,49 @@ function App() {
           openingText: movie.opening_crawl,
         };
       });
-
       setMovies(moviesData);
-
-      setIsLoading(false);
     } catch (error) {
-      console.error(error);
+      if (retrying) {
+        const timeout = setTimeout(() => {
+          fetchMovies();
+        }, 5000);
+        setRetryTimeout(timeout);
+      }
+      setError(error.message);
     }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    console.log("Clicked");
+    fetchMovies();
+  }, [fetchMovies]);
+
+  const handleOnCancel = () => {
+    setRetrying(false);
+    if (retryTimeout) {
+      clearTimeout(retryTimeout);
+    }
+  };
+
+  let content = <p>No movies found</p>;
+  if (!isLoading) {
+    if (movies.length === 0 && !error) {
+      content = <p>No movies found</p>;
+    } else if (movies.length > 0) {
+      content = <MoviesList movies={movies} />;
+    } else if (error && retrying) {
+      content = (
+        <div>
+          <p>{error}</p>
+          <button style={{ backgroundColor: "red" }} onClick={handleOnCancel}>
+            Cancel
+          </button>
+        </div>
+      );
+    }
+  } else {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -35,11 +75,7 @@ function App() {
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No movies found</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
